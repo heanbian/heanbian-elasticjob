734615c7f2a669a669e900a8edb3d2fc1e4bc2f9
mahong <mahong@dgg.net>
2019-06-14 17:00:17 +0800

    1

--- src/main/java/com/heanbian/elastic/simple/job/client/ElasticSimpleJobClient.java
+++ src/main/java/com/heanbian/elastic/simple/job/client/ElasticSimpleJobClient.java
@@ -7,12 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.core.annotation.AliasFor;
-import org.springframework.stereotype.Component;
 
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
 public @interface ElasticSimpleJobClient {
 
 	@AliasFor("cron")
